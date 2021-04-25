18927ecafcb3109f23dbdc7956fc7f594f805c32
eaert <62422332+eaert@users.noreply.github.com>
2021-04-25 23:49:38 +0300

    Validation Issues. adding validation to login page, fixing validation at signup page.

--- WelcomePage/js/form-validation.js
+++ WelcomePage/js/form-validation.js
@@ -6,12 +6,21 @@ jQuery.validator.addMethod("lettersAndNubersOnly", function(value, element) {
 	return this.optional(element) || /^[a-z0-9]+$/i.test(value);
 }, "must have letters and numbers only please");
 
+jQuery.validator.addMethod("usernameExist", function(value, element) {
+	return this.optional(element) || true; // need to add connection to main Users Array
+}, "username already exist");
+
+jQuery.validator.addMethod("passwordCurrect", function(value, element) {
+	return this.optional(element) || true; // need to add connection to main Users Array and check password
+}, "password incorrect");
+
 
 $(document).ready(function() {
-  $('#form').validate({
+  $('#signupForm').validate({
     rules: {
       userSignup: {
-          required: true
+          required: true,
+          usernameExist: false
       },
       nameSignup: {
         required: true,
@@ -30,9 +39,30 @@ $(document).ready(function() {
         required: true
       }
     },
-    registerHandler: function (form) { 
+    submitHandler: function (form) { 
       alert('valid form submitted'); 
+      $('#signupPage').css('display', 'none');
+      $('#welcomePage').css('display', 'block');
       return false; 
-  }
+  },    
+  });
+
+  $('#loginForm').validate({
+    rules: {
+      userLogin: {
+          required: true,
+          usernameExist: true
+      },
+      passwordLogin: {
+        required: true,
+        passwordCurrect: true
+      },
+    },
+    submitHandler: function (form) { 
+      // alert('valid form submitted'); 
+      // $('#loginPage').css('display', 'none');
+      // $('#settingPage').css('display', 'block');
+      return false; 
+  },    
   });
 })
